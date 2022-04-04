97f8db4829d8c089686d68c1961de3eaee8750a8
Filip Jirsák <filip@jirsak.org>
2022-04-04 23:33:21 +0200

    Initial commit

---
+++ src/main/java/cz/czechitas/intro/HlavniProgram.java
@@ -0,0 +1,15 @@
+package cz.czechitas.intro;
+
+import cz.czechitas.intro.api.*;
+import net.sevecek.util.*;
+
+public class HlavniProgram {
+
+    public void main(String[] args) {
+
+        new Fish(50, 50);
+        // TODO: Sem vepiste svuj program
+
+    }
+
+}
